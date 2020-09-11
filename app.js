945b38ce7ade48c51baa4a3a67dd562c90c2e6a5
Absylver <59806261+absylver@users.noreply.github.com>
2020-09-11 21:14:43 +0100

    Add files via upload
    updated js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const api = {
     key: "5790c40e8fea7416913444bc8bb8f228",
-    base: "https://api.openweathermap.org/data/2.5/",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
-const searchbox= document.querySelector('.search-box')
+const searchbox = document.querySelector('.search-box')
 searchbox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
@@ -28,6 +28,15 @@ function displayResults (weather) {
     let now = new Date();
     let Date = document.querySelector('.location .date');
     Date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp)}<span>&#176;c</span>`;
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weater.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}&#176;c/ ${Math.round(weather.main.temp_max)}&#176;c`
 }
 
 function dateBuilder (d) {
